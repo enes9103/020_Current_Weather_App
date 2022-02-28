ecc5d0f0a378c6b5cfc2b316b5fdce66ea3b8ba6
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2022-02-28 13:55:13 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const url = "https://api.openweathermap.org/data/2.5/"
+const key = "7d25eb467a5e851f9de46d590290c795"
+
+const setQuery = (e) => {
+    if (e.keyCode == "13") {
+        getResult(searchBar.value)
+    }
+}
+
+const getResult = (cityName) => {
+    let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`
+    fetch(query)
+    .then(weather => {
+        return weather.json()
+    })
+    .then(displayResult)
+}
+
+const displayResult = (result) => {
+    let city = document.querySelector(".city");
+    city.innerText = `${result.name}, ${result.sys.country}`
+
+    let temp = document.querySelector(".temp")
+    temp.innerText = `${Math.round(result.main.temp)}°C`
+
+    let desc = document.querySelector(".desc")
+    desc.innerText = result.weather[0].description
+
+    let minmax = document.querySelector(".minmax")
+    minmax.innerText = `${Math.round(result.main.temp_min)}°C / ${Math.round(result.main.temp_max)}°C`
+}
+
+const searchBar = document.getElementById("searchBar")
+searchBar.addEventListener("keypress", setQuery)
